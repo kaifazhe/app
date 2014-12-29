0de6d51f5c00507423b0e9c1456648ba50916dbb
jiyangmeng <147946690@qq.com>
2014-12-29 18:49:22 +0800

    创建2个模型

---
+++ ThinkPHP/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jiyangmeng
+ * Date: 14/12/29
+ * Time: 下午6:37
+ */
+define("APP_NAME","home");
+define("APP_PATH","Home/");
+require 'ThinkPHP/ThinkPHP.php';
---
+++ admin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jiyangmeng
+ * Date: 14/12/29
+ * Time: 下午6:37
+ */
+define("APP_NAME","admin");
+define("APP_PATH","Admin/");
+require 'ThinkPHP/ThinkPHP.php';
