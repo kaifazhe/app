2fa0f433e96530a710cbcccd91d1bcbc6d03a8e7
jiyangmeng <147946690@qq.com>
2014-12-30 09:13:58 +0800

    增加show文件.

--- Home/Lib/Action/IndexAction.class.php
+++ Home/Lib/Action/IndexAction.class.php
@@ -5,4 +5,21 @@ class IndexAction extends Action {
 	$this->display();
 	//$this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
     }
+    public function show(){
+        //echo "你成功访问了 INdex控制器的 Show方法".$_GET['name'];
+        $arr= array(
+            array(
+                "name"=>"aa",
+                "age"=>18,
+                "sex"=>1
+            ),
+            array(
+                "name"=>"b",
+                "age"=>18,
+                "sex"=>1
+            )
+        );
+        $this->assign("arr",$arr);
+        $this->display();
+    }
 }
